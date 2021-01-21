ff94e9e4a1bf9f51cf430bf95031e728de1c3679
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2021-01-20 19:10:08 -0500

    .

--- js/trophy.js
+++ js/trophy.js
@@ -1,10 +1,60 @@
 // Michael N. Gagnon
 
+const BADGES = {
+        "1story": {
+          img: "smile-book.png",
+          name: "1 story read",
+        },
+        "10stories": {
+          img: "vol1.png",
+          name: "10 stories read",
+        },
+        "20stories": {
+          img: "open-book.png",
+          name: "20 stories read",
+        },
+        "1 hour of reading": {
+          img:"books.png",
+          name: "1 hour of reading",
+        },
+      };
+
 const TrophyApp = {
   data() {
     return {
       counter: 0,
-      trophies: ["a", "b"],
+      trophies: [
+        "1story",
+        "10stories",
+      ],
+      /*
+      // BADGES is a constant. Making constants accessible within Vue templates seems to be an exercise in judgement.
+      // I am not particularly sophisticated when it comes to Vue, so I chose the simplest approach: defining
+      // the constant data inside with the dynamic data
+      BADGES: {
+        "1story": {
+          img: "smile-book.png",
+          name: "1 story read",
+        },
+        "10stories": {
+          img: "vol1.png",
+          name: "10 stories read",
+        },
+        "20stories": {
+          img: "open-book.png",
+          name: "20 stories read",
+        },
+        "1 hour of reading": {
+          img:"books.png",
+          name: "1 hour of reading",
+        },
+      },
+      */
+    }
+  },
+  methods: {
+    getTrophyImgUrl(tname) {
+      return `img/emojis/${BADGES[tname].img}`;
     }
   },
   mounted() {
